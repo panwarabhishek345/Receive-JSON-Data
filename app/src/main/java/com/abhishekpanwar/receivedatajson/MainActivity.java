6ef763ea17754fe65c20ec23048b319985961af9
Abhishek <panwarabhishek345@gmail.com>
2017-07-17 19:23:20 +0530

    Receive and Parse JSON Data from Internet

---
+++ app/src/main/java/com/abhishekpanwar/receivedatajson/MainActivity.java
@@ -0,0 +1,29 @@
+package com.abhishekpanwar.receivedatajson;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button click;
+    public  static TextView data;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        click = (Button) findViewById(R.id.button);
+        data = (TextView) findViewById(R.id.fetcheddata);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                  fetchData process = new fetchData();
+                  process.execute();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/abhishekpanwar/receivedatajson/fetchData.java
@@ -0,0 +1,69 @@
+package com.abhishekpanwar.receivedatajson;
+
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by Abhishek Panwar on 7/14/2017.
+ */
+
+public class fetchData extends AsyncTask<Void,Void,Void> {
+    String data ="";
+    String dataParsed = "";
+    String singleParsed ="";
+    @Override
+    protected Void doInBackground(Void... voids) {
+        try {
+            URL url = new URL("https://api.myjson.com/bins/j5f6b");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = "";
+            while(line != null){
+                line = bufferedReader.readLine();
+                data = data + line;
+            }
+
+            JSONArray JA = new JSONArray(data);
+            for(int i =0 ;i <JA.length(); i++){
+                JSONObject JO = (JSONObject) JA.get(i);
+                singleParsed =  "Name:" + JO.get("name") + "\n"+
+                                "Password:" + JO.get("password") + "\n"+
+                                "Contact:" + JO.get("contact") + "\n"+
+                                "Country:" + JO.get("country") + "\n";
+
+                dataParsed = dataParsed + singleParsed +"\n" ;
+
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        MainActivity.data.setText(this.dataParsed);
+
+    }
+}
